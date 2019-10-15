82720fae6cb3badf6a892958894b189b71dc5119
denzor200 <denismikhaylov38@gmail.com>
2019-10-16 00:22:22 +0300

    Command line parsing and one command "current" was implemented

--- sources/main.cpp
+++ sources/main.cpp
@@ -5,10 +5,32 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    // Creating QApplication and setting params..
+    QApplication app(argc, argv);
     QApplication::setOrganizationName("Denzor and Che");
     QApplication::setApplicationName("BiffTannen");
+
+    // Parsing command line..
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addOptions({
+        {{"c", "current"},
+            QCoreApplication::translate("main", "Change the current working directory."),
+            QCoreApplication::translate("main", "directory")},
+    });
+    parser.process(app);
+
+    // Initial commands handling..
+    QString targetDir = parser.value("current");
+    if (targetDir != "")
+    {
+        if (!QDir::setCurrent(targetDir))
+                qDebug() << "Could not change the current working directory";
+    }
+
+    // Creating MainWindow and executing..
     MainWindow w;
     w.show();
-    return a.exec();
+    return app.exec();
 }
